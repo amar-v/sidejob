aa58079f9b49ebb5078ed8b3a81ed2784a405937
edgolub <edis.golubic@edu.fit.ba>
2016-01-12 16:23:16 +0100

    node zeza

---
+++ public/js/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    
+/* JS Snippet for Tabs (not sure where else to put it?) */
+    $('body').on('click','.egtab-handler',function(){
+        $('.egtab-handler').removeClass('active');
+        var tabcontID = $(this).attr('data-egtabtarget');
+        $('.egtab-content').hide();
+        $('#'+tabcontID).show();
+        $(this).addClass('active');
+    });
+    
+    
+});
